39657e5205c41424bc8687d7f7e1a333e5303b1b
Hortensia <hjuniar@gmail.com>
2016-05-13 11:03:41 +0700

    nambahin connection.php

--- hjuniar/connection.php
+++ hjuniar/connection.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost";
-$username = "username";
-$password = "password";
-$dbname = "myDB";
+$username = "root";
+$password = "";
+$dbname = "codingmum";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -10,15 +10,6 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
-
-// sql to create table
-$sql = "CREATE TABLE MyGuests (
-id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
-firstname VARCHAR(30) NOT NULL,
-lastname VARCHAR(30) NOT NULL,
-email VARCHAR(50),
-reg_date TIMESTAMP
-)";
 
 if ($conn->query($sql) === TRUE) {
     echo "Table MyGuests created successfully";
--- hjuniar/index.php
+++ hjuniar/index.php
@@ -1,4 +1,5 @@
 <!DOCTYPE html>
+<?php include 'connection.php';?>
 <html lang="en">
 
 <head>
